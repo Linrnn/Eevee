8f1583a441b4f5c82b6d418d01b5e69ef8e3721f
LinRN <44719485+Linrnn@users.noreply.github.com>
2025-09-02 23:46:58 +0800

    chore：【Utils】重构“Comparable”

---
+++ Utils/Comparable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eevee.Utils
+{
+    /// <summary>
+    /// “Array.Sort”存在GC，需要“Comparison”避免GC
+    /// </summary>
+    public readonly struct Comparable<T> where T : IComparable<T>
+    {
+        public static readonly Comparison<T> Default = (lhs, rhs) => lhs.CompareTo(rhs);
+    }
+}
